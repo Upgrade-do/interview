bd8d476c71d9ead2d5def7732be705de4cf495f8
Frometa, Jose (Joaquin) <jose.frometa@one.verizon.com>
2016-06-14 12:32:16 -0400

    first commit

--- app/src/main/java/ntv/upgrade/NTSupervisor/ActivityMain.java
+++ app/src/main/java/ntv/upgrade/NTSupervisor/ActivityMain.java
@@ -202,7 +202,6 @@ public class ActivityMain extends AppCompatActivity {
         });
         vEval.setText(mHeader.getEvaluatorname());
 
-        int test = 0;
         vGrade = (Spinner) findViewById(R.id.activity_grade_spinner);
         GradeAdapterGreen gradeAdapterGreen = new GradeAdapterGreen(this);
         vGrade.setAdapter(gradeAdapterGreen);
